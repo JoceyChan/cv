128ad7f0a2c94e729a6ce948c68443d1818a16f1
JoceyChan <joceychan331718@gmail.com>
2023-06-25 23:38:41 -0700

    WIP

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
- 
-  // Activate the gliding line when a section is in view
-  window.addEventListener('scroll', function() {
-    var sections = document.querySelectorAll('section');
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+// Activate the gliding line when a section is in view
+window.addEventListener('scroll', function() {
+  var sections = document.querySelectorAll('section');
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-    sections.forEach(function(section) {
-      var topOffset = section.offsetTop;
-      var bottomOffset = topOffset + section.offsetHeight;
+  sections.forEach(function(section) {
+    var topOffset = section.offsetTop;
+    var bottomOffset = topOffset + section.offsetHeight;
 
-      if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
-        section.classList.add('active');
-      } else {
-        section.classList.remove('active');
-      }
-    });
+    if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
+      section.classList.add('active');
+    } else {
+      section.classList.remove('active');
+    }
   });
+});

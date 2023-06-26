f16038d3ac491fd1fa6c46f69ac8ffa102df20bf
JoceyChan <joceychan331718@gmail.com>
2023-06-25 21:53:32 -0700

    skills, tech, and languages

---
+++ script.js
@@ -0,0 +1,17 @@
+ 
+  // Activate the gliding line when a section is in view
+  window.addEventListener('scroll', function() {
+    var sections = document.querySelectorAll('section');
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    sections.forEach(function(section) {
+      var topOffset = section.offsetTop;
+      var bottomOffset = topOffset + section.offsetHeight;
+
+      if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    });
+  });

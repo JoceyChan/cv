d032f3aecb509a06ffec2fa9ca40aefa281dd45a
JoceyChan <joceychan331718@gmail.com>
2023-06-28 00:51:21 -0700

    One Progress Bar functioning

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // Activate the gliding line when a section is in view
+// window.addEventListener('scroll', function() {
+//   var sections = document.querySelectorAll('section');
+//   var scrollPosition = window.onscroll || document.documentElement.scrollTop;
+
+//   sections.forEach(function(section) {
+//     var topOffset = section.offsetTop;
+//     var bottomOffset = topOffset + section.offsetHeight;
+
+//     if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
+//       section.classList.add('active');
+//     } else {
+//       section.classList.remove('active');
+//     }
+//   });
+// });
+
 window.addEventListener('scroll', function() {
-  var sections = document.querySelectorAll('section');
-  var scrollPosition = window.onscroll || document.documentElement.scrollTop;
+  var progressBar = document.querySelector('.progress');
+  var windowScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var progressPercentage = (windowScroll / windowHeight) * 100;
 
-  sections.forEach(function(section) {
-    var topOffset = section.offsetTop;
-    var bottomOffset = topOffset + section.offsetHeight;
-
-    if (scrollPosition >= topOffset && scrollPosition < bottomOffset) {
-      section.classList.add('active');
-    } else {
-      section.classList.remove('active');
-    }
-  });
+  progressBar.style.width = progressPercentage + '%';
 });
